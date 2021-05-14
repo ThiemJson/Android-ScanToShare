0617422ed8b186a72594413ebe45600b7a20aeb9
nguyen_thiem <nguyen_thiem@teneocto.io>
2021-05-14 15:36:09 +0700

    recycleview, actionbottom, socical image, delete, add item

--- app/src/main/java/teneocto/thiemjason/tlu_connect/MainActivity.java
+++ app/src/main/java/teneocto/thiemjason/tlu_connect/MainActivity.java
@@ -3,13 +3,19 @@ package teneocto.thiemjason.tlu_connect;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import teneocto.thiemjason.tlu_connect.recycleview.SocialRecycleView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
+        setContentView(R.layout.activity_main);
+
+//        Intent intent = new Intent(this, SocialRecycleView.class);
+//        startActivity(intent);
     }
 }
---
+++ app/src/main/java/teneocto/thiemjason/tlu_connect/recycleview/SocialRecVAdapter.java
@@ -0,0 +1,70 @@
+package teneocto.thiemjason.tlu_connect.recycleview;
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.recyclerview.widget.RecyclerView;
+
+import teneocto.thiemjason.tlu_connect.R;
+
+public class SocialRecVAdapter extends RecyclerView.Adapter<SocialRecVAdapter.viewHolder>{
+    Context context;
+    OnItemClickListener onItemClickListener;
+
+    public SocialRecVAdapter(Context context) {
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.list_view_item, parent,false);
+        return new viewHolder(view );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+
+    public class viewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener {
+        ImageView viewHolderDelete;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            viewHolderDelete = itemView.findViewById(R.id.list_item_delete);
+            itemView.setOnClickListener(this);
+            viewHolderDelete.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == viewHolderDelete.getId()) {
+                onItemClickListener.onDelete(v, getAdapterPosition());
+            }
+        }
+    }
+
+
+    public interface OnItemClickListener {
+        void onItemClick(View view , int position);
+        void onDelete(View view, int position);
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener onItemClickListener){
+        this.onItemClickListener = onItemClickListener;
+    }
+}
